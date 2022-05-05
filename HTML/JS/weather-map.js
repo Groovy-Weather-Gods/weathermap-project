ed105137a1fc70d378cacc10809f0fb2c3760275
carlos.chavira333 <carlos.chavira333@gmail.com>
2022-05-05 15:17:42 -0500

    attempted to populate each day with a day

--- HTML/JS/weather-map.js
+++ HTML/JS/weather-map.js
@@ -1,39 +1,52 @@
 var date = document.querySelector('.date');
-var name = document.querySelector('.name');
-var desc = document.querySelector('.desc');
-var temp = document.querySelector('.temp');
-var humidity = document.querySelector('.humidity');
-var wind = document.querySelector('.wind');
-var pressure = document.querySelector('.pressure');
+// var name = document.querySelector('.name');
+// var desc = document.querySelector('.desc');
+// var temp = document.querySelector('.temp');
+// var humidity = document.querySelector('.humidity');
+// var wind = document.querySelector('.wind');
+// var pressure = document.querySelector('.pressure');
 
 
 // const OPEN_WEATHER_APPID = '3567515707a7e75d45fb9334c9856c89'
 
-$.get("http://api.openweathermap.org/data/2.5/weather", {
-    APPID: OPEN_WEATHER_APPID,
-    q:     "San Antonio, US",
+// api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
+
+$.get("http://api.openweathermap.org/data/2.5/forecast?q=london&appid="+ OPEN_WEATHER_APPID, {
+    // APPID: OPEN_WEATHER_APPID,
+    // q:     "San Antonio, US",
     units: "imperial"
 }).done(function(data) {
     console.log(data);
     var dateValue = data['dt'];
-    var nameValue = data['name'];
-    var tempValue = data['main']['temp'].toFixed(0);
-    var descValue = data['weather'][0]['description'];
-    var humidityValue = data['main']['humidity'];
-    var windValue = data['wind']['speed'].toFixed(1);
-    var pressureValue = data['main']['pressure'];
+    // var nameValue = data['name'];
+    // var tempValue = data['main']['temp'].toFixed(0);
+    // var descValue = data['weather'][0]['description'];
+    // var humidityValue = data['main']['humidity'];
+    // var windValue = data['wind']['speed'].toFixed(1);
+    // var pressureValue = data['main']['pressure'];
 
-    date.innerHTML = convertUTC(dateValue);
-    name.innerHTML = nameValue;
-    desc.innerHTML = descValue;
-    temp.innerHTML = (tempValue + "°F");
-    humidity.innerHTML = (humidityValue + "%");
-    wind.innerHTML = (windValue +"mph")
-    pressure.innerHTML = (pressureValue +"Pa");
-
-    function convertUTC() {
-        return new Date().toLocaleDateString();
+    for (var i = 0; i < 6; i++) {
+        console.log(document.getElementById('day' + (i+1)).innerHTML =  String(data.list[i].dt));
+        // console.log(typeof [i])
     }
+
+    // Object.values(data).forEach(function (value) {
+        // console.log(document.getElementById('day1').innerHTML =  data.list[i].dt);
+
+    //     console.log("this value",value)
+    // })
+
+    date.innerHTML = dateValue;
+    // name.innerHTML = nameValue;
+    // desc.innerHTML = descValue;
+    // temp.innerHTML = (tempValue + "°F");
+    // humidity.innerHTML = (humidityValue + "%");
+    // wind.innerHTML = (windValue +"mph")
+    // pressure.innerHTML = (pressureValue +"Pa");
+
+    // function convertUTC() {
+    //     return new Date(data.list[i].dt).toLocaleDateString();
+    // }
 
 });
 
