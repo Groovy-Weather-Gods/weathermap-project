bd55304a6529e8f5fe2979db8fb080fe11ce7e59
“Michael-kestler” <michael.s.kestler@gmail.com>
2022-05-06 08:52:07 -0700

    added mapbox to the html

--- HTML/JS/non-loop.js
+++ HTML/JS/non-loop.js
@@ -83,7 +83,13 @@ $('#wind5').html("Wind: " +wind5);
 });
 
 
-
+// mapboxgl.MAPBOX_KEY = MAPBOX_KEY;
+// const map = new mapboxgl.Map({
+//     container: 'map', // container ID
+//     style: 'mapbox://styles/mapbox/streets-v11', // style URL
+//     center: [-86.1934114, 39.741091], // starting position [lng, lat]
+//     zoom: 9 // starting zoom
+// });
 
 
 
